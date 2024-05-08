24c11e22d894d8a07d4ff5f2e1d1b4708b7d65b7
TogAr2 <59421074+TogAr2@users.noreply.github.com>
2024-05-09 01:51:19 +0200

    Cleanup

--- src/main/java/io/github/togar2/pvp/feature/CombatConfiguration.java
+++ src/main/java/io/github/togar2/pvp/feature/CombatConfiguration.java
@@ -10,11 +10,10 @@ import java.util.List;
 import java.util.Set;
 
 public class CombatConfiguration {
-	private final Set<CombatFeature> features = new HashSet<>();
-	private final Set<LazyFeatureInit> lazyFeatures = new HashSet<>();
+	private final Set<ConstructableFeature> features = new HashSet<>();
 	
 	public CombatConfiguration add(IndependentFeature feature) {
-		features.add(feature);
+		features.add(new ConstructedFeature(feature));
 		return this;
 	}
 	
@@ -37,18 +36,18 @@ public class CombatConfiguration {
 			}
 		}
 		
-		lazyFeatures.add(new LazyFeatureInit(constructor.getDeclaringClass(), constructor, parameterTypes));
+		features.add(new LazyFeatureInit(constructor, parameterTypes));
 		return this;
 	}
 	
 	public CombatFeatureSet build() {
-		List<CombatFeature> result = new ArrayList<>(features);
+		List<CombatFeature> result = new ArrayList<>(features.size());
 		System.out.println("build start");
 		
-		List<LazyFeatureInit> buildOrder = getBuildOrder();
-		for (LazyFeatureInit feature : buildOrder) {
+		List<ConstructableFeature> buildOrder = getBuildOrder();
+		for (ConstructableFeature feature : buildOrder) {
 			try {
-				result.add(construct(feature));
+				result.add(feature.construct());
 			} catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
 				throw new RuntimeException(e);
 			}
@@ -58,42 +57,23 @@ public class CombatConfiguration {
 		return new CombatFeatureSet(result.toArray(CombatFeature[]::new));
 	}
 	
-	private CombatFeature construct(LazyFeatureInit feature) throws InvocationTargetException,
-																	InstantiationException, IllegalAccessException {
-		Object[] initArgs = new Object[feature.dependsOn.length];
-		
-		for (int i = 0; i < feature.dependsOn.length; i++) {
-			Class<?> dependClass = feature.dependsOn[i];
-			CombatFeature dependFeature = getFeatureOf(dependClass);
-			if (dependFeature != null) {
-				initArgs[i] = dependFeature;
-			} else {
-				LazyFeatureInit dependLazyInit = getLazyFeatureOf(dependClass);
-				assert dependLazyInit != null;
-				initArgs[i] = construct(dependLazyInit);
-			}
-		}
-		
-		return feature.constructor.newInstance(initArgs);
-	}
-	
 	/**
 	 * Performs a (recursive) topological sort to make sure all the features
 	 * that are depended on by other features are first in the list
 	 *
 	 * @return the list with ordering
 	 */
-	private List<LazyFeatureInit> getBuildOrder() {
-		List<LazyFeatureInit> order = new ArrayList<>(lazyFeatures.size());
-		Set<LazyFeatureInit> visiting = new HashSet<>();
+	private List<ConstructableFeature> getBuildOrder() {
+		List<ConstructableFeature> order = new ArrayList<>(features.size());
+		Set<ConstructableFeature> visiting = new HashSet<>();
 		
 		while (true) {
 			// Find unprocessed feature
-			LazyFeatureInit unprocessed = null;
-			for (LazyFeatureInit lazyFeature : lazyFeatures) {
-				if (order.contains(lazyFeature)) continue;
-				if (visiting.contains(lazyFeature)) continue;
-				unprocessed = lazyFeature;
+			ConstructableFeature unprocessed = null;
+			for (ConstructableFeature feature : features) {
+				if (order.contains(feature)) continue;
+				if (visiting.contains(feature)) continue;
+				unprocessed = feature;
 			}
 			
 			if (unprocessed == null) break;
@@ -103,27 +83,29 @@ public class CombatConfiguration {
 		return order;
 	}
 	
-	private void visit(List<LazyFeatureInit> order, Set<LazyFeatureInit> visiting, LazyFeatureInit current) {
+	private void visit(List<ConstructableFeature> order, Set<ConstructableFeature> visiting, ConstructableFeature current) {
 		if (order.contains(current)) return; // Feature has already been added
 		if (visiting.contains(current)) throw new RuntimeException("Configuration has a recursive dependency");
 		
-		visiting.add(current);
-		
-		for (Class<?> dependClass : current.dependsOn) {
-			if (getFeatureOf(dependClass) != null) continue;
-			LazyFeatureInit lazyDepend = getLazyFeatureOf(dependClass);
-			if (lazyDepend == null) throw new RuntimeException("A feature of type " + dependClass.getTypeName()
-					+ " is required for " + current.featureClass.getTypeName());
+		if (current instanceof LazyFeatureInit lazy) {
+			visiting.add(current);
 			
-			visit(order, visiting, lazyDepend);
+			for (Class<?> dependClass : lazy.dependsOn) {
+				ConstructableFeature dependFeature = getFeatureOf(dependClass);
+				if (dependFeature == null) throw new RuntimeException("A feature of type " + dependClass.getTypeName()
+						+ " is required for " + current.featureClass.getTypeName());
+				
+				visit(order, visiting, lazy);
+			}
+			
+			visiting.remove(current);
 		}
 		
-		visiting.remove(current);
 		order.add(current);
 	}
 	
-	private @Nullable CombatFeature getFeatureOf(Class<?> clazz) {
-		for (CombatFeature feature : features) {
+	private @Nullable ConstructableFeature getFeatureOf(Class<?> clazz) {
+		for (ConstructableFeature feature : features) {
 			if (clazz.isAssignableFrom(feature.getClass()))
 				return feature;
 		}
@@ -131,14 +113,52 @@ public class CombatConfiguration {
 		return null;
 	}
 	
-	private @Nullable LazyFeatureInit getLazyFeatureOf(Class<?> clazz) {
-		for (LazyFeatureInit feature : lazyFeatures) {
-			if (clazz.isAssignableFrom(feature.featureClass))
-				return feature;
+	private sealed abstract static class ConstructableFeature {
+		private final Class<?> featureClass;
+		
+		public ConstructableFeature(Class<?> featureClass) {
+			this.featureClass = featureClass;
 		}
 		
-		return null;
+		abstract CombatFeature construct() throws InvocationTargetException, InstantiationException, IllegalAccessException;
 	}
 	
-	private record LazyFeatureInit(Class<?> featureClass, Constructor<CombatFeature> constructor, Class<?>[] dependsOn) {}
+	static final class ConstructedFeature extends ConstructableFeature {
+		private final CombatFeature feature;
+		
+		private ConstructedFeature(CombatFeature feature) {
+			super(feature.getClass());
+			this.feature = feature;
+		}
+		
+		@Override
+		CombatFeature construct() {
+			return feature;
+		}
+	}
+	
+	final class LazyFeatureInit extends ConstructableFeature {
+		private final Constructor<CombatFeature> constructor;
+		private final Class<?>[] dependsOn;
+		
+		public LazyFeatureInit(Constructor<CombatFeature> constructor, Class<?>[] dependsOn) {
+			super(constructor.getDeclaringClass());
+			this.constructor = constructor;
+			this.dependsOn = dependsOn;
+		}
+		
+		@Override
+		CombatFeature construct() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+			Object[] initArgs = new Object[dependsOn.length];
+			
+			for (int i = 0; i < dependsOn.length; i++) {
+				Class<?> dependClass = dependsOn[i];
+				ConstructableFeature dependFeature = getFeatureOf(dependClass);
+				assert dependFeature != null;
+				initArgs[i] = dependFeature.construct();
+			}
+			
+			return constructor.newInstance(initArgs);
+		}
+	}
 }
