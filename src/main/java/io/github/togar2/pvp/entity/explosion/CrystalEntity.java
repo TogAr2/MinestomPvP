ed8e7ee04b633534519530c72d56f4a91840a8ea
kingsthere <kingsthere0@hotmail.com>
2024-10-10 19:09:10 +0800

    Fix CrystalEntity explosion

--- src/main/java/io/github/togar2/pvp/entity/explosion/CrystalEntity.java
+++ src/main/java/io/github/togar2/pvp/entity/explosion/CrystalEntity.java
@@ -5,7 +5,10 @@ import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.LivingEntity;
 import net.minestom.server.entity.damage.Damage;
 import net.minestom.server.entity.metadata.other.EndCrystalMeta;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.NotNull;
 
 public class CrystalEntity extends LivingEntity {
@@ -36,16 +39,18 @@ public class CrystalEntity extends LivingEntity {
 		if (isInvulnerable() || isImmune(damage.getType())) {
 			return false;
 		}
-		
+
 		// Set the last damage type since the event is not cancelled
 		this.lastDamage = damage;
-		
+
+		// Save this.instance locally
+		Instance instance = this.instance;
 		remove();
 		if (instance.getExplosionSupplier() != null
 				&& !DamageTypeInfo.of(damage.getType()).explosive()) {
 			instance.explode((float) position.x(), (float) position.y(), (float) position.z(), 6.0f);
 		}
-		
+
 		return true;
 	}
 }
